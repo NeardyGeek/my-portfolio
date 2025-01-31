cfd0fafb6735b49418f06249fb757c2cd8a853fa
Gen Li <genli@Gens-MacBook-Pro.local>
2025-01-31 01:01:43 -0800

    add new links

--- src/app/components/Contacts/Contacts.jsx
+++ src/app/components/Contacts/Contacts.jsx
@@ -7,41 +7,41 @@ const Contacts = () => {
     <>
       <section id="contact">
         <div className="flex flex-col xl:flex-row justify-center items-center p-20 gap-5 font-Inconsolata">
-          <div className="flex flex-col gap-4 p-6 flex-1 items-center xl:items-start  xl:ml-[35px]"> 
+          <div className="flex flex-col gap-4 p-6 flex-1 items-center xl:items-start  xl:ml-[35px]">
             <img className="size-12" src="/logo.png" alt="Logo" />
             <p className="text-[#F0E3CA] text-xl text-center xl:text-left">
               let's create your next experience together
             </p>
           </div>
-          <div className="flex flex-col items-start gap-5 flex-wrap flex-1 xl:ml-[300px]">
+          <div className="flex flex-col items-start gap-5 flex-wrap flex-1 xl:ml-[800px]">
             <h1 className="text-white text-3xl font-extrabold">Contacts</h1>
             <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
               <div className="flex justify-center items-center gap-4">
-                <Image
-                  src="/mail.png"
-                  width={32}
-                  height={32}
-                  alt="image not found"
-                />
-                <Link
+                <a
                   className="text-white text-xl text-center"
                   href="mailto:gen.li2@outlook.com"
+                  target="_blank"
                 >
-                  gen.li2@outlook.com
-                </Link>
+                  <Image
+                    src="/mail.png"
+                    width={32}
+                    height={32}
+                    alt="image not found"
+                  />
+                </a>
               </div>
               <div className="flex justify-center items-center gap-4">
-                <Image
-                  src="/linkedIn logo.png"
-                  width={32}
-                  height={32}
-                  alt="image not found"
-                />
                 <Link
                   className="text-white text-2xl text-center"
                   href="https://www.linkedin.com/in/gen-li1024/"
+                  target="_blank"
                 >
-                  Li Gen
+                  <Image
+                    src="/linkedIn logo.png"
+                    width={32}
+                    height={32}
+                    alt="image not found"
+                  />
                 </Link>
               </div>
             </div>
--- src/app/components/My_Work/MyWork.jsx
+++ src/app/components/My_Work/MyWork.jsx
@@ -4,42 +4,63 @@ import Image from "next/image";
 const MyWork = () => {
   return (
     <>
-    <section id="services">
-      <div className="flex flex-col justify-center items-center font-Inconsolata  gap-12 py-52">
-        <div className="flex justify-center items-center">
-          <h1 className="text-white text-6xl font-semibold">My Work</h1>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
-          <div className="relative">
-            <Image src="/demo sim swap.gif" width={300} height={335} alt="work" />
-            <div className="bg-transparent absolute top-0 flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
-              <h1 className="text-pink-800/65 text-base font-semibold">
-               account change
-              </h1>
+      <section id="services">
+        <div className="flex flex-col justify-center items-center font-Inconsolata  gap-12 py-52">
+          <div className="flex justify-center items-center">
+            <h1 className="text-white text-6xl font-semibold">My Work</h1>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
+            <a
+              className="relative"
+              href="https://play.google.com/store/apps/details?id=com.tmobile.services.nameid&hl=en_US"
+              target="_blank"
+            >
+              <Image
+                src="/demo scam shield.gif"
+                width={300}
+                height={325}
+                alt="work"
+              />
+              <div className="bg-transparent absolute top-0 flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
+                <h1 className="text-pink-800/65 text-base font-semibold">
+                  T-Mobile Scam Shield
+                </h1>
+              </div>
+            </a>
 
-            </div>
-          </div>
+            <a
+              className="relative"
+              href="https://play.google.com/store/apps/details?id=com.tmobile.vvm.application&hl=en_US"
+              target="_blank"
+            >
+              <Image
+                src="/demo sorting.gif"
+                width={300}
+                height={325}
+                alt="work"
+              />
+              <div className="bg-transparent absolute top-0 flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
+                <h1 className="text-pink-800/65 text-base font-semibold">
+                  T-Mobile Visual Voicemail
+                </h1>
+              </div>
+            </a>
 
-          <div className="relative">
-            <Image src="/demo sorting.gif" width={300} height={325} alt="work" />
-            <div className="bg-transparent absolute top-0 flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
-              <h1 className="text-pink-800/65 text-base font-semibold">
-                sorting
-              </h1>
-            </div>
-          </div>
-     
-         
-          <div className="relative">
-            <Image src="/demo jap.gif" width={300} height={325} alt="work" />
-            <div className="bg-transparent absolute top-0 flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
-              <h1 className="text-pink-800/65 text-base font-semibold">
-                left menu
-              </h1>
+            <div className="relative">
+              <Image
+                src="/demo sim swap.gif"
+                width={300}
+                height={325}
+                alt="work"
+              />
+              <div className="bg-transparent absolute top-0 flex flex-col justify-center items-center py-8 px-4  gap-4 opacity-95">
+                <h1 className="text-pink-800/65 text-base font-semibold">
+                  left menu
+                </h1>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </section>
     </>
   );
